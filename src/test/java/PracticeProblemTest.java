baedd16826ec9820ca58c3adbdba893d8d876837
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-04-03 12:42:07 -0400

    Update PracticeProblemTest.java

--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -2,6 +2,8 @@ import java.lang.reflect.Method;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 import java.io.*;
+import java.util.*;
+
 public class PracticeProblemTest {
 @Test
 @DisplayName("")
